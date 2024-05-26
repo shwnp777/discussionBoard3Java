fd82eb11e720c11565ad44030a5fab40fb08e3ac
Shawn-Patrick <34813563+shwnp777@users.noreply.github.com>
2024-05-26 14:04:21 -0700

    Java Commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Example with Array
+        int[] intArray = new int[5]; // Fixed size array of integers
+        intArray[0] = 10;
+        intArray[1] = 20;
+        intArray[2] = 30;
+        intArray[3] = 40;
+        intArray[4] = 50;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Accessing array elements
+        System.out.println("Array elements:");
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+
+        // Example with ArrayList
+        ArrayList<Integer> intArrayList = new ArrayList<>(); // Dynamic ArrayList of integers
+        intArrayList.add(10);
+        intArrayList.add(20);
+        intArrayList.add(30);
+        intArrayList.add(40);
+        intArrayList.add(50);
+
+        // Accessing ArrayList elements
+        System.out.println("ArrayList elements:");
+        for (int i = 0; i < intArrayList.size(); i++) {
+            System.out.println(intArrayList.get(i));
+        }
+
+        // Demonstrating dynamic resizing of ArrayList
+        intArrayList.add(60);
+        System.out.println("ArrayList after adding an element:");
+        for (int i = 0; i < intArrayList.size(); i++) {
+            System.out.println(intArrayList.get(i));
         }
     }
 }
